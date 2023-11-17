3768043299e9b797424c58b78117ecf7c761e860
sanekee <5906708+sanekee@users.noreply.github.com>
2023-11-17 14:00:35 +0800

    feat: export context functions

--- context.go
+++ context.go
@@ -6,6 +6,12 @@ import (
 	"github.com/sanekee/limi/internal/limi"
 )
 
+// GetURLParam get value set by label matched in url
 func GetURLParam(ctx context.Context, key string) string {
 	return limi.GetURLParam(ctx, key)
+}
+
+// GetURLParam set data to the value set by label matched in url
+func ParseURLParam(ctx context.Context, key string, data any) error {
+	return limi.ParseURLParam(ctx, key, data)
 }
